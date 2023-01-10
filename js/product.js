75fdf7b41bba68bf8d33e33344dee6a21e717d99
Jelson J <jelson20@outlook.com>
2023-01-10 15:49:23 +0000

    updated app file

---
+++ js/product.js
@@ -0,0 +1,82 @@
+const products = [
+	{
+		id: 1,
+		name: 'Louis Vitton',
+		price: 29.99,
+		instock: 100,
+		date: 'June, 01 2020',
+		comment: 52,
+		category: 'Bag',
+		imgSrc: './images/bag3.png'
+	},
+	{
+		id: 2,
+		name: 'MacBook Pro',
+		price: 24.99,
+		instock: 43,
+		date: 'April, 11, 2021',
+		comment: 62,
+		category: 'Technology',
+		imgSrc: './images/macbook-air.png'
+	},
+	{
+		id: 3,
+		name: 'Nike',
+		price: 19.99,
+		instock: 10,
+		date: 'May, 20, 2020',
+		comment: 152,
+		category: 'Shoes',
+		imgSrc: './images/nike-1.png'
+	},
+	{
+		id: 4,
+		name: 'Air Jordan Nike',
+		price: 25.99,
+		instock: 5,
+		date: 'July, 19, 2022',
+		comment: 72,
+		category: 'Shoes',
+		imgSrc: './images/nike-1.png'
+	},
+	{
+		id: 5,
+		name: 'Louis Vitton Brown',
+		price: 29.99,
+		instock: 4,
+		date: 'March, 08, 2021',
+		comment: 82,
+		category: 'Bag',
+		imgSrc: './images/bag-4.png'
+	},
+	{
+		id: 6,
+		name: 'Mac',
+		price: 39.99,
+		instock: 40,
+		date: 'January, 05 2023',
+		comment: 52,
+		category: 'Technology',
+		imgSrc: './images/apple-mac.png'
+	},
+	{
+		id: 7,
+		name: 'Mac',
+		price: 39.99,
+		instock: 40,
+		date: 'January, 05 2023',
+		comment: 52,
+		category: 'Technology',
+		imgSrc: './images/apple-mac.png'
+	},
+	{
+		id: 8,
+		name: 'Mac',
+		price: 39.99,
+		instock: 40,
+		date: 'January, 05 2023',
+		comment: 52,
+		category: 'Technology',
+		imgSrc: './images/apple-mac.png'
+	}
+];
